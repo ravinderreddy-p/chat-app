d7bd9cad11a86237121a5ceb7cf56ce400d680f5
Ravi <ravinder9.reddy@gmail.com>
2021-05-15 00:53:49 +0530

    enabled jwt authentication

--- main/app.py
+++ main/app.py
@@ -1,67 +1,116 @@
-from flask import Flask, jsonify, request, flash
-from flask_login import LoginManager, logout_user, login_user, current_user, login_required
-from main.config import Config
+import uuid
+from datetime import datetime, timedelta
+from functools import wraps
+import jwt
+from flask import Flask, jsonify, request, make_response
+from flask_login import LoginManager, logout_user, login_required
 from main.models import db_setup, User, db
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
-app.config.from_object(Config)
+app.config['SECRET_KEY'] = 'thisissecretkey'
 db_setup(app)
 login_manager = LoginManager(app)
 login_manager.init_app(app)
-# login_manager.login_view = 'login'
 
 
-@app.route('/')
-@login_required
-def health_check():
-    return jsonify(
-        {
-            'message': 'I am healthy!'
-        }
+# decorator for verifying the JWT
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        # jwt is passed in the request header
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+        # return 401 if token is not passed
+        if not token:
+            return jsonify({'message': 'Token is missing !!'}), 401
+
+        try:
+            # decoding the payload to fetch the stored details
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+            current_user = User.query.filter_by(public_id=data['public_id']).first()
+        except:
+            return jsonify({
+                'message': 'Token is invalid !!'
+            }), 401
+        # returns the current logged in users contex to the routes
+        return f(current_user, *args, **kwargs)
+
+    return decorated
+
+
+@app.route('/user', methods=['GET'])
+@token_required
+def get_all_users(current_user):
+    users = User.query.all()
+    output = []
+    for user in users:
+        output.append({
+            'public_id': user.public_id,
+            'name': user.name,
+            'email': user.email
+        })
+    return jsonify({'users': output})
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    auth = request.form
+
+    if not auth or not auth.get('email') or not auth.get('password'):
+        return make_response(
+            'Could not verify',
+            401,
+            {'WWW-Authenticate': 'Basic realm = "Login required !!"'}
+        )
+
+    user = User.query.filter_by(email=auth.get('email')).first()
+
+    if not user:
+        return make_response(
+            'Could not verify',
+            401,
+            {'WWW-Authenticate': 'Basic realm = "User does not exist !!"'}
+        )
+
+    if check_password_hash(user.password, auth.get('password')):
+        token = jwt.encode({
+            'public_id': user.public_id,
+            'exp': datetime.utcnow() + timedelta(minutes=30)
+        }, app.config['SECRET_KEY'])
+        return make_response(token)
+    return make_response(
+        'Could not verify',
+        403,
+        {'WWW-Authenticate': 'Basic realm ="Wrong Password !!"'}
     )
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(user_id)
 
 
 @app.route('/signup', methods=['POST'])
 def register_user():
-    data = request.get_json()
-    username = data.get('username')
+    data = request.form
+    name = data.get('name')
     email = data.get('email')
     password = data.get('password')
-    user = User(
-        username=username,
-        email=email
-    )
-    user.set_password(password)
-    db.session.add(user)
-    db.session.commit()
-    return jsonify({
-        'success': True,
-        'User': username
-    })
 
+    user = User.query.filter_by(email=email).first()
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    username = request.form['username']
-    password = request.form['password']
-    user = User.query.filter_by(username=username).first()
-    if user is None or not user.check_password(password):
-        flash('Invalid user name or password')
-        return "Invalid credentials or unauthorized access"
-
-    login_user(user)
-    print(current_user.username)
-    return 'user login successful'
+    if not user:
+        user = User(
+            public_id=str(uuid.uuid4()),
+            name=name,
+            email=email,
+            password=generate_password_hash(password)
+        )
+        db.session.add(user)
+        db.session.commit()
+        return make_response('Successfully registered.', 201)
+    else:
+        return make_response('User already exists. Please login', 202)
 
 
 @app.route('/logout')
-@login_required
-def logout():
-    logout_user()
+@token_required
+def logout(current_user):
     return 'User logged out successfully'
-
--- main/models.py
+++ main/models.py
@@ -1,7 +1,6 @@
 import os
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin, LoginManager, login_manager
 
 DATABASE_URL = 'postgresql://{}/{}'.format('localhost:5432', 'chatapp_db')
@@ -19,17 +18,10 @@ def db_setup(app, database_path=DATABASE_URL):
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
+    public_id = db.Column(db.String(50), unique=True)
+    name = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
+    password = db.Column(db.String(128))
 
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
-    def __repr__(self):
-        return '<User {}>.format(self.username)'
 
 
